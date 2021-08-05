10e34cda305f7db8f5ff84409f4d045aa8d5e07b
Maxim Manylov <manylov@gmail.com>
2021-08-05 16:20:30 +0300

    fix pgstorage module

--- src/modules/storage/PgStorage.ts
+++ src/modules/storage/PgStorage.ts
@@ -1,40 +1,45 @@
-import pgPromise, { ColumnSet, IConnected, IDatabase, IMain } from 'pg-promise';
-import { inject, injectable, postConstruct }                  from 'inversify';
-import { Environment }                                        from '../environment/Environment';
-import { ILogger }                                            from '../logger/ILogger';
-import { IClient }                                            from 'pg-promise/typescript/pg-subset';
+import pgPromise, { IConnected, IDatabase, IMain } from 'pg-promise'
+import { inject, injectable, postConstruct } from 'inversify'
+import { Environment } from '../environment/Environment'
+import { ILogger } from '../logger/ILogger'
+import { IClient } from 'pg-promise/typescript/pg-subset'
+
+type QueryType = { text: string; values?: any[] }
 
 @injectable()
 export class PgStorage {
-
-  private readonly pgp: IMain;
-  private readonly db: IDatabase<any>;
-  private connection: IConnected<any, IClient> | undefined;
+  private readonly pgp: IMain
+  private readonly db: IDatabase<any>
+  private connection: IConnected<any, IClient> | undefined
 
   public constructor(
     @inject(Environment) protected environment: Environment,
-    @inject(ILogger) protected logger: ILogger,
+    @inject(ILogger) protected logger: ILogger
   ) {
     this.pgp = pgPromise({
-      capSQL: true,
-    });
+      capSQL: true
+    })
 
     this.db = this.pgp({
       host: this.environment.DB_HOST,
       user: this.environment.DB_USER,
       database: this.environment.DB_NAME,
       password: this.environment.DB_PASSWORD,
-      port: parseInt(this.environment.DB_PORT, 10),
-    });
+      port: parseInt(this.environment.DB_PORT, 10)
+    })
   }
 
   @postConstruct()
   public async initialize() {
-    this.connection = await this.db.connect();
+    this.connection = await this.db.connect()
   }
 
   public async close() {
-    this.logger.info(`Closing DB connection`);
-    await this.connection!.done();
+    this.logger.info(`Closing DB connection`)
+    await this.connection!.done()
+  }
+
+  public async query(query: QueryType): Promise<any> {
+    return this.db.query(query)
   }
 }
