20754e78ca407049be5232fbc2caf858ca367380
Maxim Manylov <manylov@gmail.com>
2021-08-05 16:22:51 +0300

    add extrinsics module

---
+++ src/services/extrinsics/extrinsics.types.ts
@@ -0,0 +1,36 @@
+import { BlockNumber, EventRecord } from '@polkadot/types/interfaces'
+import { Compact, Vec } from '@polkadot/types'
+import { GenericExtrinsic } from '@polkadot/types/extrinsic/Extrinsic'
+import { createInterface } from '@modules/common/functions/createInterface'
+
+export const IExtrinsicsService = createInterface<IExtrinsicsService>('IExtrinsicsService')
+
+export interface IExtrinsicsService {
+  extractExtrinsics(
+    eraId: number,
+    sessionId: number,
+    blockNumber: Compact<BlockNumber>,
+    events: Vec<EventRecord>,
+    extrinsicsVec: Vec<GenericExtrinsic>
+  ): Promise<void>
+}
+
+export interface IExtrinsic {
+  id: string
+  block_id: Compact<BlockNumber>
+  parent_id?: string
+  session_id: number
+  era: number
+  section: any
+  method: any
+  mortal_period: number | null
+  mortal_phase: number | null
+  is_signed: boolean
+  signer: string | null
+  tip: number
+  nonce: number
+  ref_event_ids: string | null
+  version: number
+  extrinsic: any
+  args: any
+}
