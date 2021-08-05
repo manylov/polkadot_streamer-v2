dbc00d1dc4f8ad7fa7122e59929a8a256b9c7555
Maxim Manylov <manylov@gmail.com>
2021-08-05 16:19:04 +0300

    fix Environment modue

--- src/modules/environment/Environment.ts
+++ src/modules/environment/Environment.ts
@@ -1,69 +1,59 @@
-import appRoot                       from 'app-root-path';
-import { injectable, postConstruct } from 'inversify';
-import {
-  plainToClass,
-  Transform
-}                                    from 'class-transformer';
-import {
-  IsNotEmpty,
-  IsNumber,
-  IsPort,
-  IsString,
-  Min,
-  MinLength,
-  validateOrReject
-}                                    from 'class-validator';
-import { container }                 from '../../container';
-import { ILogger }                   from '../logger/ILogger';
-import dotenv                        from 'dotenv';
-import { resolve }                   from 'path';
+import appRoot from 'app-root-path'
+import { injectable, postConstruct } from 'inversify'
+import { plainToClass } from 'class-transformer'
+import { IsNotEmpty, IsPort, IsString, MinLength, validateOrReject } from 'class-validator'
+import { container } from '../../container'
+import { ILogger } from '../logger/ILogger'
+import dotenv from 'dotenv'
+import { resolve } from 'path'
 
-dotenv.config({path: resolve(appRoot.path, '.env'), debug: true});
+dotenv.config({ path: resolve(appRoot.path, '.env'), debug: true })
 
 @injectable()
 export class Environment {
-
   @IsNotEmpty()
   @IsString()
   @MinLength(2)
-  public DB_HOST!: string;
+  public DB_HOST!: string
 
   @IsString()
   @MinLength(3)
-  public DB_USER!: string;
+  public DB_USER!: string
 
   @IsString()
   @MinLength(3)
-  public DB_PASSWORD!: string;
+  public DB_PASSWORD!: string
 
   @IsNotEmpty()
   @MinLength(2)
-  public DB_NAME!: string;
+  public DB_NAME!: string
 
   @IsPort()
-  public DB_PORT: string = '5432';
+  public DB_PORT = '5432'
 
   @IsString()
-  public LOG_LEVEL: string = 'info';
+  public LOG_LEVEL = 'info'
 
-  @IsNumber()
-  @Min(5 * 60)
-  @Transform(({value}) => parseInt(value, 10), {toClassOnly: true})
-  public DB_DUMMY_WRITE_INTERVAL_SECONDS: number = 30 * 60;
+  @IsNotEmpty()
+  SUBSTRATE_URI!: string
 
-  public static create() {
-    return plainToClass(this, process.env);
+  @IsString()
+  DB_SCHEMA = 'dot_polka'
+
+  public static create(): Environment {
+    return plainToClass(this, process.env)
   }
 
   @postConstruct()
-  public async initialize() {
+  public async initialize(): Promise<void> {
     try {
-      await validateOrReject(this, {validationError: {target: false, value: false}});
+      await validateOrReject(this, { validationError: { target: false, value: false } })
     } catch (validationErrors) {
-      const logger = container.get(ILogger);
-      validationErrors
-        .forEach((error: object) => logger.fatal(`Bad environment variable(s): %o`, error));
-      process.exit(1);
+      const logger = container.get(ILogger)
+      validationErrors.forEach((error: Record<string, unknown>) =>
+        logger.fatal(`Bad environment variable(s): %o`, error)
+      )
+      process.exit(1)
     }
   }
 }
