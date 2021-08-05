20c4d6014f056a01a12bf6b51b36484acb9f9d3d
Maxim Manylov <manylov@gmail.com>
2021-08-05 16:17:54 +0300

    fix startup files

--- src/container.ts
+++ src/container.ts
@@ -1,28 +1,55 @@
-import 'reflect-metadata';
-import { Container }   from 'inversify';
-import { Environment } from './modules/environment/Environment';
-import { Logger }      from './modules/logger/Logger';
-import { ILogger }     from './modules/logger/ILogger';
-import { PgStorage }   from './modules/storage/PgStorage';
-import { App }         from './modules/main/App';
+import 'reflect-metadata'
+import { Container } from 'inversify'
 
-export const container = new Container();
+import { Environment } from '@modules/environment/Environment'
+import { Logger, ILogger } from '@modules/logger'
+import { PgStorage } from '@modules/storage/PgStorage'
+import { App } from '@modules/main/App'
+import { PolkadotModule, IPolkadotModule } from '@modules/polkadot'
 
-container.bind(ILogger).to(Logger).inSingletonScope();
+import { BlocksService, IBlocksService } from '@services/blocks'
+import { ConfigService, IConfigService } from '@services/config'
+import { ConsumerService, IConsumerService } from '@services/consumer'
+import { ExtrinsicsService, IExtrinsicsService } from '@services/extrinsics'
+import { RunnerService, IRunnerService } from '@services/runner'
+import { StakingService, IStakingService } from '@services/staking'
+import { WatchdogService, IWatchdogService } from '@services/watchdog'
 
-container.bind(Environment)
-  .toDynamicValue(async (context) => {
-    const env = Environment.create();
-    await env.initialize();
-    return env;
+import { IBlockRepository, BlockRepository } from '@repositories/blocks'
+import { IConfigRepository, ConfigRepository } from '@repositories/config'
+import { IEraRepository, EraRepository } from '@repositories/era'
+import { IEventRepository, EventRepository } from '@repositories/event'
+import { IExtrinsicRepository, ExtrinsicRepository } from '@repositories/extrinsic'
+
+export const container = new Container()
+
+container.bind(ILogger).to(Logger).inSingletonScope()
+container.bind(IPolkadotModule).to(PolkadotModule).inSingletonScope()
+container.bind(IBlocksService).to(BlocksService).inSingletonScope()
+container.bind(IConfigService).to(ConfigService).inSingletonScope()
+container.bind(IConsumerService).to(ConsumerService).inSingletonScope()
+container.bind(IExtrinsicsService).to(ExtrinsicsService).inSingletonScope()
+container.bind(IRunnerService).to(RunnerService).inSingletonScope()
+container.bind(IStakingService).to(StakingService).inSingletonScope()
+container.bind(IWatchdogService).to(WatchdogService).inSingletonScope()
+container.bind(IBlockRepository).to(BlockRepository).inSingletonScope()
+container.bind(IConfigRepository).to(ConfigRepository).inSingletonScope()
+container.bind(IEraRepository).to(EraRepository).inSingletonScope()
+container.bind(IEventRepository).to(EventRepository).inSingletonScope()
+container.bind(IExtrinsicRepository).to(ExtrinsicRepository).inSingletonScope()
+
+container
+  .bind(Environment)
+  .toDynamicValue(async () => {
+    const env = Environment.create()
+    await env.initialize()
+    return env
   })
   .inSingletonScope()
   .onActivation((context, env) => {
-    context.container.get(ILogger).setLevel(env.LOG_LEVEL);
-    return env;
-  });
+    context.container.get(ILogger).setLevel(env.LOG_LEVEL)
+    return env
+  })
 
-container.bind(PgStorage).toSelf().inSingletonScope();
-container.bind(App).toSelf().inSingletonScope();
-
-
+container.bind(App).toSelf().inSingletonScope()
+container.bind(PgStorage).toSelf().inSingletonScope()
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
-import 'reflect-metadata';
-import { container } from './container';
-import { App }       from './modules/main/App';
-import { ILogger }   from './modules/logger/ILogger';
-
-(async () => {
-  const logger = await container.get<ILogger>(ILogger);
+import 'reflect-metadata'
+import { container } from './container'
+import { App } from './modules/main/App'
+import { ILogger } from './modules/logger/ILogger'
+;(async () => {
+  const logger = container.get<ILogger>(ILogger)
 
   try {
-    const app = await container.getAsync(App);
+    const app = await container.getAsync(App)
 
     process.on('SIGINT', () => {
-      logger.info('SIGINT');
-      app.close();
-      process.exit(1);
-    });
+      logger.info('SIGINT')
+      app.close()
+      process.exit(1)
+    })
 
     process.on('SIGTERM', () => {
-      logger.info('SIGTERM');
-      app.close();
-      process.exit(1);
-    });
+      logger.info('SIGTERM')
+      app.close()
+      process.exit(1)
+    })
 
-    await app.start();
+    await app.start()
   } catch (err) {
-    logger.fatal(err.message, err);
-    process.exit(1);
+    logger.fatal(err.message, err)
+    process.exit(1)
   }
-
-})();
+})()
