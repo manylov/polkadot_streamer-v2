b38cd6aa2ef8f37d113bfeed28334d1e9fbe618e
Maxim Manylov <manylov@gmail.com>
2021-08-05 16:18:18 +0300

    init knex

---
+++ src/modules/database/knexfile.ts
@@ -0,0 +1,56 @@
+require('dotenv').config()
+
+const { CLIENT, DATABASE, PG_USER, PASSWORD, HOST, PG_PORT } = process.env
+
+module.exports = {
+  development: {
+    client: CLIENT,
+    connection: {
+      database: DATABASE,
+      user: PG_USER,
+      password: PASSWORD,
+      host: HOST,
+      port: PG_PORT
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/migrations',
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+}
